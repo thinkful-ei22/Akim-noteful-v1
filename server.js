d507c18ca70d4be4429fe23fee2204629b2e3e76
Akim Desire <adesi006.ng@gmail.com>
2018-06-25 18:59:39 -0400

    added search handling functionality

--- server.js
+++ server.js
@@ -9,8 +9,29 @@ const app = express();
 app.use(express.static('public'));
 
 app.get('/api/notes', (req, res) => {
+    let queryString = req.query.searchTerm;
+    if(queryString !== undefined) {
+        res.json(
+            data.filter(item =>
+            item.title.includes(queryString)
+            ||item.content.includes(queryString)
+        )
+        );
+    }
+
+    else {
     res.json(data);
+    }
+    console.log(req.query);
 });
+
+app.get('/api/notes/:id', (req, res) => {
+    const id = req.params.id;
+    const queryString = req.query.searchTerm
+    res.json(data.find(item => item.id === parseInt(id)));
+    console.log('with ID');
+});
+
 
 app.listen(8080, function () {
   console.info(`Server listening on ${this.address().port}`);
